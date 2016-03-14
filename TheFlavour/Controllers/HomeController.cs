3c34167dc6f73922303e9deebce8abd9cf826283
Maxim Penderuk <Maxim Penderuk>
2016-03-14 03:32:23 +0200

    Working slider for `Home/Index.cshtml`

--- TheFlavour/Controllers/HomeController.cs
+++ TheFlavour/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,16 @@ namespace TheFlavour.Controllers
     {
         public ActionResult Index()
         {
+            // Path to images we need for slider.
+            string filePath = Server.MapPath(@"~/images/Slides/");
+            DirectoryInfo imgDir = new DirectoryInfo(filePath);
+            List<string> imageNames = new List<string>();
+            foreach (var file in imgDir.GetFiles("*.jpg"))
+            {
+                imageNames.Add(file.Name);
+            }
+            ViewBag.ImageNames = imageNames;
+
             return View();
         }
 
